68674fa449a1336665892e8aa452ffb36dd872e9
Jose Antonio Insua <insuafer@adobe.com>
2024-03-24 17:12:40 +0100

    Test new repo key

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s $(pwd)/vimrc ~/.vimrc 
+ln -s $(pwd)/vimrc ~/.vimrc
 
